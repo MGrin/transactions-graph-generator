5b1f6248171da1d18ab337f6b54d8231cb9df964
ChrisJBlake <christopher@semandex.net>
2020-06-15 16:52:32 -0400

    Exclude ATMs in Transactions

--- generator/utils.py
+++ generator/utils.py
@@ -1,10 +1,10 @@
 import time
 
-logFile = open('logs/' + time.strftime("%H.%M.%S_%d-%m-%Y") + '.log', 'a+')
+#logFile = open('logs/' + time.strftime("%H.%M.%S_%d-%m-%Y") + '.log', 'a+')
 
 def writeBatch(file, batch): file.write('\n'.join(batch) + '\n')
 
 def log(str = ''):
   line = time.strftime("%d/%m/%Y %H:%M:%S") + ": " + str
   print(line)
-  logFile.write(line  + '\n')
+  #logFile.write(line  + '\n')
--- models/Transaction.py
+++ models/Transaction.py
@@ -1,30 +1,25 @@
 from mimesis import Datetime, Numbers, Text, Business
-from random import random
-from numpy import random as npr
+import random
 from math import ceil
 from uuid import uuid4
 from .Node import Node
 
+
 class Transaction(Node):
-	_datetime = Datetime()
-	_numbers = Numbers()
-	_text = Text()
-	_business = Business()
+    _datetime = Datetime()
+    _business = Business()
 
-	def __init__(self, sourceId, targetId):
-		self.__type = 'Transaction'
-		self.id = uuid4()
-		self.source = sourceId
-		self.target = targetId
-		self.date = self._datetime.date(start=2015, end=2019)
-		self.time = self._datetime.time()
+    def __init__(self, sourceId: int, targetId: int):
+        self.__type = "Transaction"
+        self.id = uuid4()
+        self.source = sourceId
+        self.target = targetId
+        self.datetime = self._datetime.datetime(start=2000, end=2020)
+        # self.time = self._datetime.time()
 
-		if random() < 0.05:
-			self.amount = self._numbers.float_number(100000, 1000000)
-		else:
-			self.amount = npr.exponential(10)
+        self.amount = random.randint(1, 10000)
+        if random.random() < 0.15:
+            self.currency = self._business.currency_iso_code()
+        else:
+            self.currency = None
 
-		if random() < 0.15:
-			self.currency = self._business.currency_iso_code()
-		else:
-			self.currency = None
